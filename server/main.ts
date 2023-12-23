22c3942129376a61a462b15aed50ec0944a18503
JuanM04 <me@juanm04.com>
2023-12-23 00:53:39 -0300

    Persist document

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,26 @@
 import { Hocuspocus } from "@hocuspocus/server";
 import { Logger } from "@hocuspocus/extension-logger";
+import { Doc } from "yjs";
+
+const documents = new Map<string, Doc>();
 
 const server = new Hocuspocus({
   port: 1234,
   extensions: [new Logger()],
   debounce: 0,
+  async onStoreDocument(data) {
+    documents.set(data.documentName, data.document);
+  },
+
+  async onLoadDocument(data): Promise<Doc> {
+    if (documents.has(data.documentName)) {
+      return documents.get(data.documentName)!;
+    } else {
+      const doc = new Doc();
+      documents.set(data.documentName, doc);
+      return doc;
+    }
+  },
 });
 
 server.listen();
